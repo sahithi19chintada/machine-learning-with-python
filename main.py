04eed25f34e33c567217a0b370ad590c0c7a35ba
sahithi19chintada <chsahithi17@gmail.com>
2025-05-17 13:04:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+from RPS_game import play
+from RPS import player
+from RPS_game import quincy
+
+# Uncomment to auto-run test
+# import test_module
+
+play(player, quincy, 1000, verbose=True)
